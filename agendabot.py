303bb68ccc402253237e3a5e5ab1542067888055
v0idpwn <v0idpwn@gmail.com>
2019-04-09 17:35:13 -0300

    Add agendabot.py

---
+++ agendabot.py
@@ -0,0 +1,54 @@
+# Imports
+import requests
+from datetime import datetime, timedelta
+
+# Configs
+matricula = "1337"
+senha = "sample"
+RU = 2
+refeicoes= ["café", "almoço", "jantar"]
+modo = "semanal"
+date = datetime.today()
+
+# Os restaurantes tem ids internos
+RU_1 = 1
+RU_2 = 41
+RU_SELECTED = RU_1 if RU == 1 else RU_2
+
+# Faz login no portal, retorna a sessão obtida
+def login(matricula, senha):
+    target = "https://portal.ufsm.br/ru/j_security_check"
+    login_data = {'j_username': matricula, 'j_password': senha}
+    sessao = requests.session()
+    sessao.post(target, login_data)
+    return sessao
+
+
+# Recebe os dados de um agendamento e o faz
+def agenda(sessao, refeicao, ru, inicio, fim):
+    inicio = inicio.strftime("%d/%m/%Y")
+    fim = fim.strftime("%d/%m/%Y")
+
+    target = "https://portal.ufsm.br/ru/usuario/agendamento/form.html"
+    dados = {'periodo.fim': fim, 'periodo.inicio': inicio, 'restaurante': ru, 'tiposRefeicao': refeicao}
+    sessao.post(target, dados);
+
+
+# Fazendo login
+s = login(matricula, senha)
+
+# Adquirindo datas para modo
+if(modo == "semanal"):
+    inicio = date + timedelta(days=1)
+    fim = date + timedelta(days=5)
+else:
+    inicio = date + timedelta(days=1)
+    fim = date + timedelta(days=1)
+
+# Fazendo as requisições
+if "café" in refeicoes:
+    agenda(s, 1, RU_1, inicio, fim)
+if "almoço" in refeicoes:
+    agenda(s, 2, RU_SELECTED, inicio, fim)
+if "jantar" in refeicoes:
+    agenda(s, 3, RU_1, inicio, fim)
